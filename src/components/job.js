26d5051736736b89c53768e5f6cbf82c256d6631
Germán Rojas <g.rojasc@uniandes.edu.co>
2024-09-28 18:52:21 -0500

    Add internationalization support for job listings

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,37 @@
 import React from "react";
+import { FormattedDate, FormattedNumber } from "react-intl";
 
-const Job = (props) => {
+const Job = ({ offer, locale }) => {
+  const salaryFormat = () => {
+    if (offer.salary >= 1) {
+      return locale.startsWith("es")
+        ? offer.salary === 1
+          ? "1 millón"
+          : `${offer.salary} millones`
+        : `${offer.salary} million`;
+    }
+    return offer.salary;
+  };
+
   return (
     <tr>
-      <th scope="row">{props.offer.id}</th>
-      <td>{props.offer.name}</td>
-      <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
-      <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <th scope="row">{offer.id}</th>
+      <td>{offer.name}</td>
+      <td>{offer.company}</td>
+      <td>{salaryFormat()}</td>
+      <td>{offer.city}</td>
+      <td>
+        <FormattedDate
+          value={new Date(offer.date)}
+          year="numeric"
+          month="long"
+          day="numeric"
+          weekday="long"
+        />
+      </td>
+      <td>
+        <FormattedNumber value={offer.visits} />
+      </td>
     </tr>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import JobsList from "./components/jobslist";
+import { IntlProvider } from "react-intl";
+import localEsMessages from "./locales/es";
+import localEnMessages from "./locales/en";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const userLocale = navigator.language;
+const messages = userLocale.startsWith("es") ? localEsMessages : localEnMessages;
+
+ReactDOM.render(
+  <IntlProvider locale={userLocale} messages={messages}>
+    <JobsList locale={userLocale} />
+  </IntlProvider>,
+  document.getElementById("root")
+);
